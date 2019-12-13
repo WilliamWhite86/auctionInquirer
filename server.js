75da127e0ef59cbdaa8da0380e2d696090d88208
Shakur <Shakur@Shakurs-MacBook-Air.local>
2019-12-13 00:58:36 -0500

    working inquirer and mysql connected to database, please use your password in the password section

--- server.js
+++ server.js
@@ -1,1 +1,70 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 
+
+function inq() {
+
+
+
+
+    inquirer
+        .prompt(
+            [
+
+                {
+                    type: "list",
+
+                    name: "Post an Item ",
+
+                    message: "What would you like to post?",
+
+                    choices: ["car", "bed", "girlfriend"]
+
+                },
+                {
+                    type: "list",
+
+                    name: "BID an Item ",
+
+                    message: "What would you like to BID?",
+
+                    choices: ["car", "bed", "girlfriend"]
+
+
+                }
+            ])
+        .then(answers => {
+            console.log("its working");
+
+
+        });
+
+};
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Vip2004.",
+    database: "auction_db"
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+
+
+    console.log("Database is connected!");
+    connection.end();
+});
+
+inq();
+
+// connection.query("SELECT * FROM products WHERE flavor=?;", [flavor], (err, res) => {
+//     if (err) {
+//         throw err;
+//     }
+//     res.forEach((row) => {
+//         console.log(`The price for ${flavor} is ${row.price}`);
+//     });
+// });
